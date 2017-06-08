f78ff69eb097a418259d4addb8ff27f9a935167c
Mark Danley <mdanley62@gmail.com>
2017-06-08 11:34:49 -0500

    Updated character main page to use TextInputLayout

--- app/src/main/java/com/madinnovations/rmu/view/activities/character/CharacterMainPageFragment.java
+++ app/src/main/java/com/madinnovations/rmu/view/activities/character/CharacterMainPageFragment.java
@@ -1,17 +1,17 @@
-/**
- * Copyright (C) 2016 MadInnovations
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+  Copyright (C) 2016 MadInnovations
+  <p/>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  <p/>
+  http://www.apache.org/licenses/LICENSE-2.0
+  <p/>
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  */
 package com.madinnovations.rmu.view.activities.character;
 
@@ -55,6 +55,7 @@ import com.madinnovations.rmu.view.di.modules.CharacterFragmentModule;
 import com.madinnovations.rmu.view.utils.Boast;
 import com.madinnovations.rmu.view.utils.EditTextUtils;
 import com.madinnovations.rmu.view.utils.SpinnerUtils;
+import com.madinnovations.rmu.view.utils.TextInputLayoutUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -66,7 +67,8 @@ import javax.inject.Inject;
 /**
  * Handles interactions with the UI for character creation.
  */
-public class CharacterMainPageFragment extends Fragment implements EditTextUtils.ValuesCallback, SpinnerUtils.ValuesCallback {
+public class CharacterMainPageFragment extends Fragment implements EditTextUtils.ValuesCallback, SpinnerUtils.ValuesCallback,
+		TextInputLayoutUtils.ValuesCallback {
 	@SuppressWarnings("unused")
 	private static final String TAG = "CharacterMainPageFrag";
 	private static final String DRAG_SWAP_STATS = "swap-stats";
@@ -134,13 +136,13 @@ public class CharacterMainPageFragment extends Fragment implements EditTextUtils
 		campaignSpinner = new SpinnerUtils<Campaign>().initSpinner(layout, getActivity(), campaignRxHandler.getAll(), this,
 				R.id.campaign_spinner, null);
 		campaignSpinner.setEnabled(charactersFragment.getCurrentInstance().getCurrentLevel() == 0);
-		firstNameEdit = EditTextUtils.initEdit(layout, getActivity(), this, R.id.first_name_edit,
-				R.string.validation_character_first_name_required);
-		lastNameEdit = EditTextUtils.initEdit(layout, getActivity(), this, R.id.last_name_edit,
-				R.string.validation_character_last_name_required);
-		knownAsEdit = EditTextUtils.initEdit(layout, getActivity(), this, R.id.known_as_edit,
-				R.string.validation_character_known_as_required);
-		descriptionEdit = EditTextUtils.initEdit(layout, getActivity(), this, R.id.notes_edit,
+		firstNameEdit = TextInputLayoutUtils.initEdit(layout, getActivity(), this, R.id.first_name_textInputLayout,
+				R.id.first_name_edit, R.string.validation_character_first_name_required);
+		lastNameEdit = TextInputLayoutUtils.initEdit(layout, getActivity(), this, R.id.last_name_textInputLayout,
+													 R.id.last_name_edit, R.string.validation_character_last_name_required);
+		knownAsEdit = TextInputLayoutUtils.initEdit(layout, getActivity(), this, R.id.known_as_textInputLayout,
+													R.id.known_as_edit, R.string.validation_character_known_as_required);
+		descriptionEdit = TextInputLayoutUtils.initEdit(layout, getActivity(), this, R.id.notes_textInputLayout, R.id.notes_edit,
 				R.string.validation_character_description_required);
 		raceSpinnerUtils = new SpinnerUtils<>();
 		raceSpinnerUtils.initSpinner(layout, getActivity(), raceRxHandler.getAll(), this, R.id.race_spinner, null);
---
+++ app/src/main/java/com/madinnovations/rmu/view/utils/TextInputLayoutUtils.java
@@ -0,0 +1,147 @@
+/*
+  Copyright (C) 2016 MadInnovations
+  <p>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  <p>
+  http://www.apache.org/licenses/LICENSE-2.0
+  <p>
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+package com.madinnovations.rmu.view.utils;
+
+import android.content.Context;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Generic EditText handler code
+ */
+public final class TextInputLayoutUtils {
+
+	/**
+	 * Initializes an EditText with afterTextChanged and onFocusChange method implementations.
+	 *
+	 * @param layout  the layout that contains the EditText to be initialized
+	 * @param context  an android Context that can be used to obtain resources, etc.
+	 * @param valuesCallback  an implementation of the {@link ValuesCallback} interface that can be used to update the object
+	 *                        backing the UI
+	 * @param textLayoutId  the resource ID of the TextInputLayout to be initialized
+	 * @param editTextId  the resource ID of the EditText to be initialized
+	 * @param errorStringId  the resource ID of a string that can be used to display an error when the EditText is empty. And id
+	 *                       of 0 indicates that an empty EditText is not an error
+	 * @return  the {@link EditText} instance that was initialized or null if not found.
+	 */
+	public static EditText initEdit(@NonNull View layout, @NonNull final Context context,
+									@NonNull final ValuesCallback valuesCallback, @IdRes final int textLayoutId,
+									@IdRes final int editTextId, @StringRes final int errorStringId) {
+		TextInputLayout textInputLayout = null;
+		if(textLayoutId != 0) {
+			textInputLayout = (TextInputLayout) layout.findViewById(textLayoutId);
+		}
+		final EditText editText = (EditText)layout.findViewById(editTextId);
+		if(textInputLayout != null && editText != null) {
+			initEdit(textInputLayout, editText, context, valuesCallback, textLayoutId, editTextId, errorStringId);
+		}
+
+		return editText;
+	}
+
+	/**
+	 * Initializes an EditText with afterTextChanged and onFocusChange method implementations.
+	 *
+	 * @param textInputLayout  the TextInputLayout instance to use
+	 * @param editText  the EditText instance to use
+	 * @param context  an android Context that can be used to obtain resources, etc.
+	 * @param valuesCallback  an implementation of the {@link ValuesCallback} interface that can be used to update the object
+	 *                        backing the UI
+	 * @param textLayoutId  the resource ID of the TextInputLayout to be initialized
+	 * @param editTextId  the resource ID of the EditText to be initialized
+	 * @param errorStringId  the resource ID of a string that can be used to display an error when the EditText is empty. And id
+	 *                       of 0 indicates that an empty EditText is not an error
+	 */
+	public static void initEdit(final TextInputLayout textInputLayout, @NonNull final EditText editText,
+								@NonNull final Context context, @NonNull final ValuesCallback valuesCallback,
+								@IdRes final int textLayoutId, @IdRes final int editTextId,
+								@StringRes final int errorStringId) {
+		editText.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+			@Override
+			public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+			@Override
+			public void afterTextChanged(Editable editable) {
+				if (textInputLayout != null) {
+					if (editable.length() == 0 && errorStringId != 0) {
+						textInputLayout.setError(context.getString(errorStringId));
+						editText.setError(context.getString(errorStringId));
+					}
+					else {
+						textInputLayout.setErrorEnabled(false);
+					}
+				}
+			}
+		});
+		editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View view, boolean hasFocus) {
+				if (!hasFocus) {
+					if(errorStringId == 0 || editText.getText().length() > 0) {
+						String newName = editText.getText().toString();
+						String oldName = valuesCallback.getValueForEditText(editTextId);
+
+						if (!newName.equals(oldName)) {
+							valuesCallback.setValueFromEditText(editTextId, newName);
+						}
+					}
+				}
+			}
+		});
+		editText.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View view) {
+				return valuesCallback.performLongClick(editTextId);
+			}
+		});
+	}
+
+	/**
+	 * Defines methods used to get and set the value of the backing object.
+	 */
+	public interface ValuesCallback {
+		/**
+		 * Gets the current value of the field in the backing object.
+		 *
+		 * @param editTextId  the resource id of the EditText instance
+		 * @return  the current value of the field in the backing object as a String.
+		 */
+		String getValueForEditText(@IdRes int editTextId);
+
+		/**
+		 * Sets the current value of the field in the backing object.
+		 *
+		 * @param editTextId  the resource id of the EditText instance
+		 * @param newString  the new value of the field as a String
+		 */
+		void setValueFromEditText(@IdRes int editTextId, String newString);
+
+		/**
+		 * Called when a long click occurs for the given EditText.
+		 *
+		 * @param editTextId  the resource id of the EditText instance that was long clicked
+		 * @return  true if the long click was handled, otherwise false.
+		 */
+		boolean performLongClick(@IdRes int editTextId);
+	}
+}
